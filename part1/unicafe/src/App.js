762a0d700e4d6998543a86deb17a7e74b9c14934
AUVTE0 <66396373+AUVTE0@users.noreply.github.com>
2023-03-29 14:44:34 +0100

    exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -13,9 +13,12 @@ const App = () => {
       <button onClick = {() => setNeutral(neutral+1)}>neutral</button>
       <button onClick = {() => setBad(bad+1)}>bad</button>
       <Header text = 'statistics'/>
-      <Part name = 'good' total={good}/>
-      <Part name = 'neutral' total={neutral}/>
-      <Part name = 'bad' total={bad}/>
+      <Part name = 'good' value = {good}/>
+      <Part name = 'neutral' value = {neutral}/>
+      <Part name = 'bad' value = {bad}/>
+      <Part name = 'all' value = {good + neutral + bad}/>
+      <Part name = 'average' value = {(good + bad*-1)/(good + neutral + bad)}/>
+      <Part name = 'positive' value = {String(good/(good + neutral + bad)*100) + '%'} />
     </div>
   )
 }
@@ -23,7 +26,7 @@ const App = () => {
 
 const Header =  props => <h1>{props.text}</h1>
 
-const Part = props => <p>{props.name} {props.total}</p>
+const Part = props => <p>{props.name} {props.value}</p>
 
 
 export default App
