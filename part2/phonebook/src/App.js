ab15bd6197cd06e51045e9b35626759ba3317477
AUVTE0 <66396373+AUVTE0@users.noreply.github.com>
2023-03-31 15:19:01 +0100

    exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-123456'}
-  ]) 
+  
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(res => {
+        setPersons(res.data)
+      })
+  
+  })
 
   const addName = (e) => {
     e.preventDefault()
