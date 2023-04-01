5964510655707eea3b5cd4c7a7c27cbab25e9b27
AUVTE0 <66396373+AUVTE0@users.noreply.github.com>
2023-04-01 13:53:43 +0100

    exercise 2.20

--- part2/countries/src/services/weatherService.js
+++ part2/countries/src/services/weatherService.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
-const key = "771565397ab9d20a6e501929790c4dc4"
+const key = process.env.REACT_APP_API_KEY
 
 const get = ([lat, lon]) => {
+    console.log(key)
     const req = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`)
     return req.then(res => res.data)
 }
