a807be444792ae9e9af5b0632742d82166e3c10c
AUVTE0 <66396373+AUVTE0@users.noreply.github.com>
2023-08-13 17:33:12 +0100

    exercise 9.9

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,13 +1,16 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-app.get("/ping", (_req: Request, res: Response) => {
+app.get("/api/ping", (_req: Request, res: Response) => {
+    console.log("received ping requuest")
     res.send("pong");
 });
 
-const PORT = 3003;
+const PORT = 3001;
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
