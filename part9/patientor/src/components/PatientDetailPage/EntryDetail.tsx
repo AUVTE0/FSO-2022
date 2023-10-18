3d3633ca3819db7667231c45049af3653f9f0c1a
AUVTE0 <66396373+AUVTE0@users.noreply.github.com>
2023-10-18 16:08:01 +0100

    exercise 9.24

--- part9/patientor/src/components/PatientDetailPage/EntryDetail.tsx
+++ part9/patientor/src/components/PatientDetailPage/EntryDetail.tsx
@@ -1,13 +1,32 @@
-import { Entry } from "../../types";
+import { useEffect, useState } from "react";
+import { Entry, Diagnosis } from "../../types";
+import diagnosisService from "../../services/diagnoses";
 
-const EntryDetail = ({entry}: {entry: Entry}) => (
-    <div>
-        {entry.date}
-        <i> {entry.description}</i>
-        <ul>
-            {entry.diagnosisCodes?.map(code => <li>{code}</li>)}
-        </ul>
-    </div>
-);
+
+const EntryDetail = ({entry}: {entry: Entry}) => {
+    const [diagnoses, setDiagnoses] = useState<Diagnosis[]>();
+    useEffect(() => {
+        const fetchDiagnoses = async () => {
+          const diagnoses = await diagnosisService.getAll();
+          setDiagnoses(diagnoses);
+        };
+        void fetchDiagnoses();
+      }, []);
+
+    const diagnosisName = (code: string) => (
+        diagnoses?.find(item => item.code === code)?.name
+    );
+      
+    return (
+        <div>
+            {entry.date}
+            <i> {entry.description}</i>
+            <ul>
+                {entry.diagnosisCodes?.map(code => <li>{code} {diagnosisName(code)}</li>)}
+            </ul>
+        </div>
+    );
+};
+
 
 export default EntryDetail;
