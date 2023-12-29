adc0c0f94bc0d20fd02a91f17444f4a777a1e58c
AUVTE0 <66396373+AUVTE0@users.noreply.github.com>
2023-12-29 15:07:16 +0000

    exercise 8.3

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -102,10 +102,18 @@ const typeDefs = `
     genres: [String!]!
   }
 
+  type Author {
+    name: String!
+    id: ID!
+    born: Int!
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -114,6 +122,10 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors.map(author => ({
+      ...author,
+      bookCount: books.filter(book => book.author === author.name).length
+    }))
   }
 }
 
